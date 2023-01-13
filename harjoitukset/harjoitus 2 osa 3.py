364633464c954467bda838fd6bc3e8a79bf31f3c
Chad <teemu.forssell@metropolia.fi>
2023-01-13 14:30:30 +0200

    harjoitus2

--- harjoitukset/harjoitus 2 osa 3.py
+++ harjoitukset/harjoitus 2 osa 3.py
@@ -1,6 +1,12 @@
 suorakulmiokanta = input("Anna suorakulmion kanta: ")
 suorakulmiopituus = input("Anna suorakulmion pituus: ")
-suorakulmiokanta2 = float(suorakulmiokanta)
-suorakulmiopituus2 = float(suorakulmiopituus)
+suorakulmiokanta = float(suorakulmiokanta)
+suorakulmiopituus = float(suorakulmiopituus)
+pintaala=suorakulmiopituus*suorakulmiokanta
+kehä=suorakulmiokanta*2+suorakulmiopituus*2
+print("Suorakulmion pinta-ala on " + str(pintaala) + " ja suorakulmion kehä on " + str(kehä))
+
+
+
 
 
