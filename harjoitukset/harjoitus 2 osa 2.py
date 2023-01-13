47cd692fbde7bf97b83ba81c48607aac7ceb3a31
Chad <teemu.forssell@metropolia.fi>
2023-01-13 15:26:08 +0200

    harjoitus2

--- harjoitukset/harjoitus 2 osa 2.py
+++ harjoitukset/harjoitus 2 osa 2.py
@@ -1,6 +1,8 @@
 import math
-säde = input("Anna ympyrän säde: ")
+säde = input("Anna ympyrän säde (m): ")
 säde2 = float(säde)
 vastaus = (säde2)**2*math.pi
-print("Ympyrän pinta-ala: " + str(vastaus))
+print(f"Ympyrän pinta-ala: {vastaus:.2f} neliömetriä")
+
+
 
