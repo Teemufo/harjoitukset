93721efa22220bdcdaee0947507c68d2f590f798
Chad <teemu.forssell@metropolia.fi>
2023-01-13 15:16:41 +0200

    harjoitus2

--- harjoitukset/harjoitus 2 osa 2.py
+++ harjoitukset/harjoitus 2 osa 2.py
@@ -1,6 +1,6 @@
 import math
 säde = input("Anna ympyrän säde: ")
 säde2 = float(säde)
-vastaus = (säde2)*2*math.pi
+vastaus = (säde2)**2*math.pi
 print("Ympyrän pinta-ala: " + str(vastaus))
 
--- harjoitukset/harjoitus 2 osa 4.py
+++ harjoitukset/harjoitus 2 osa 4.py
@@ -7,6 +7,6 @@ luku3 = int(luku3)
 summa = luku1+luku2+luku3
 tulo = luku1*luku2*luku3
 keskiarvo = (luku1+luku2+luku3)/3
-print(f"Kokonaislukujen summa on {summa} ,tulo on {tulo} ja keskiarvo on {keskiarvo:.2f}")
+print(f"Kokonaislukujen summa on {summa}, tulo on {tulo} ja keskiarvo on {keskiarvo:.2f}")
 
 
