6d051ae1f60336ed819948b02e081e6b1a03df1d
Chad <teemu.forssell@metropolia.fi>
2023-02-02 19:26:38 +0200

    harjoitus 7

--- harjoitukset/moduuli07/harjoitus 7 osa 3.py
+++ harjoitukset/moduuli07/harjoitus 7 osa 3.py
@@ -2,8 +2,8 @@
 #uuden lentoaseman, hakea jo syötetyn lentoaseman tiedot vai lopettaa. Jos käyttäjä valitsee uuden lentoaseman syöttämisen,
 #ohjelma kysyy käyttäjältä lentoaseman ICAO-koodin ja nimen. Jos käyttäjä valitsee haun, ohjelma kysyy ICAO-koodin ja
 #tulostaa sitä vastaavan lentoaseman nimen. Jos käyttäjä haluaa lopettaa, ohjelman suoritus päättyy. Käyttäjä saa valita
-#uuden toiminnon miten monta kertaa tahansa aina siihen asti, kunnes hän haluaa lopettaa. (ICAO-koodi on lentoaseman
-#yksilöivä tunniste. Esimerkiksi Helsinki-Vantaan lentoaseman ICAO-koodi on EFHK. Löydät koodeja helposti selaimen avulla.)
+#uuden toiminnon miten monta kertaa tahansa aina siihen asti, kunnes hän haluaa lopettaa. ICAO-koodi on lentoaseman
+#yksilöivä tunniste. Esimerkiksi Helsinki-Vantaan lentoaseman ICAO-koodi on EFHK. Löydät koodeja helposti selaimen avulla.
 
 def lisää():
     tunnus = input("Anna lentoaseman tunnus: ")
