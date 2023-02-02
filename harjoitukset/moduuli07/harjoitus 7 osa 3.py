84d91d9c1039f1ef665840c607422324ca6d60ae
Chad <teemu.forssell@metropolia.fi>
2023-02-02 19:25:58 +0200

    harjoitus 6

--- harjoitukset/moduuli07/harjoitus 7 osa 3.py
+++ harjoitukset/moduuli07/harjoitus 7 osa 3.py
@@ -5,44 +5,28 @@
 #uuden toiminnon miten monta kertaa tahansa aina siihen asti, kunnes hän haluaa lopettaa. (ICAO-koodi on lentoaseman
 #yksilöivä tunniste. Esimerkiksi Helsinki-Vantaan lentoaseman ICAO-koodi on EFHK. Löydät koodeja helposti selaimen avulla.)
 
-#funktio lisää lentoaseman sanakirjaan
-
 def lisää():
-    print("vaiheessa")
     tunnus = input("Anna lentoaseman tunnus: ")
     nimi = input("Anna lentoaseman nimi: ")
-    # lisätään uusi lentoasema sanakirjaan
     lentoasemat[tunnus] = nimi
     return
-
-#funktio hakee lentoaseman nimen ICAO-koodin perusteella sanakirjasta
 
 def hae():
     koodi = input("Anna lentoaseman koodi: ")
     if koodi in lentoasemat:
-        print(f"{koodi}")
+        print(f"{lentoasemat[koodi]}")
         return
-
-def tulosta():
-
-#luon sanakirjan jolle annan yhden alkion
 
 lentoasemat = {"EFHK" : "Helsinki-Vantaan lentoasema"}
 
-toiminto = -1 #annetaan sellainen alkuarvo että päästään toiston sisään
+toiminto = 0
 
-while toiminto != 3:
-    print("0 = Tulosta lentoasemien tiedot.")
-    print("1 = Lisää uusi lentoasema.")
-    print("2 = Hae lentoasema.")
-    print("3 = Lopetus.")
+while toiminto != 2:
+    print("0 = Lisää uusi lentoasema.")
+    print("1 = Hae lentoasema.")
+    print("2 = Lopetus.")
     toiminto = int(input("Valitse toiminto: "))
     if toiminto == 0:
-        tulosta()
-    elif toiminto == 1:
         lisää()
-    elif toiminto == 2:
+    elif toiminto == 1:
         hae()
-# TODO lisää toiminnot
-
-print("Kiitos ja näkemiin.")
